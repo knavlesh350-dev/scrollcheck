481a0b4916ad6009f17f5c7cec41ac7abe48be29
knavlesh350-dev <knavlesh350@gmail.com>
2026-08-23 00:03:22 -0700

    Create script.js - JavaScript functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+// ScrollCheck - Main JavaScript
+
+function startTracking() {
+    alert('Welcome to ScrollCheck! Tracking feature coming soon.');
+    console.log('Start Tracking button clicked');
+}
